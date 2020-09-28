e6d208aafd60bc0b53b7bf903661bda87c587c9a
Neil Williams <n.williams@primeoak.co.uk>
2020-09-28 11:10:17 +0100

    menu and base colours set

--- mpj-designers/header.php
+++ mpj-designers/header.php
@@ -1,24 +1,34 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
 <?php wp_head(); ?>
 </head>
+
 <body <?php body_class(); ?>>
+
 <div id="wrapper" class="hfeed">
+
 <header id="header">
-<div id="branding">
-<div id="site-title">
-<?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; } ?>
-<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?>" rel="home"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a>
-<?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1>'; } ?>
-</div>
-<div id="site-description"><?php bloginfo( 'description' ); ?></div>
-</div>
-<nav id="menu">
-<div id="search"><?php get_search_form(); ?></div>
-<?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
-</nav>
+
+    <nav id="menu" class="bg-primary">
+
+        <div class="main-menu wrapper">
+
+            <div class="site-logo">
+
+                <img src="/wp-content/uploads/montpelier-joinery-logo.png" alt="Montpelier Joinery Logo" class="logo">
+
+            </div>
+
+            <?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
+
+        </div>   
+            
+    </nav>
+
 </header>
+
 <div id="container">
